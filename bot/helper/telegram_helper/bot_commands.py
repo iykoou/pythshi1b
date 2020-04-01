b0fbdbb17a417aed0c77115b90529b4473eb32b0
shivamaria <61585707+shivamaria@users.noreply.github.com>
2020-04-01 23:14:04 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,18 +1,18 @@
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = 'start2'
-        self.MirrorCommand = 'mirror2'
-        self.TarMirrorCommand = 'tarmirror2'
-        self.CancelMirror = 'cancel2'
-        self.CancelAllCommand = 'cancelall2'
-        self.ListCommand = 'list2'
-        self.StatusCommand = 'status2'
-        self.AuthorizeCommand = 'authorize2'
-        self.UnAuthorizeCommand = 'unauthorize2'
-        self.PingCommand = 'ping2'
-        self.StatsCommand = 'stats2'
-        self.HelpCommand = 'help2'
-        self.LogCommand = 'log2'
+        self.StartCommand = 'start@shivamvj1_bot'
+        self.MirrorCommand = 'mirror@shivamvj1_bot'
+        self.TarMirrorCommand = 'tarmirror@shivamvj1_bot'
+        self.CancelMirror = 'cancel@shivamvj1_bot'
+        self.CancelAllCommand = 'cancelall@shivamvj1_bot'
+        self.ListCommand = 'list@shivamvj1_bot'
+        self.StatusCommand = 'status@shivamvj1_bot'
+        self.AuthorizeCommand = 'authorize@shivamvj1_bot'
+        self.UnAuthorizeCommand = 'unauthorize@shivamvj1_bot'
+        self.PingCommand = 'ping@shivamvj1_bot'
+        self.StatsCommand = 'stats@shivamvj1_bot'
+        self.HelpCommand = 'help@shivamvj1_bot'
+        self.LogCommand = 'log@shivamvj1_bot'
 
 
 BotCommands = _BotCommands()
